156b230055f760b941736f83574337a71a8f9ec1
haygrouve <haygrouve@gmail.com>
2022-08-22 16:54:59 +0300

    add no prediction check

--- src/components/predictions-modal/predictionsModal.component.tsx
+++ src/components/predictions-modal/predictionsModal.component.tsx
@@ -15,15 +15,22 @@ const PredictionsModal: React.FC<IpredictionsModal> = ({ data, close }) => {
     close();
     clearTimeout(timeout);
   }, 15000);
+
   return createPortal(
     <div
       style={{ top: `${data.y}px`, left: `${data.x - 250}px` }}
       className={styles.modal}
     >
-      <p>{data.name}</p>
-      <p>1: {data.home}</p>
-      <p>X: {data.draw}</p>
-      <p>2: {data.away}</p>
+      {data.name || data.home || data.draw || data.away ? (
+        <>
+          <p>{data.name}</p>
+          <p>1: {data.home}</p>
+          <p>X: {data.draw}</p>
+          <p>2: {data.away}</p>
+        </>
+      ) : (
+        <p>No data</p>
+      )}
       <button onClick={() => close()}>close</button>
     </div>,
     portalRoot
